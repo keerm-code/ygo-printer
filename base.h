dc88f1ae0bdde469306566ccfb40e360609abbc7
咯卡 <20206815@stu.neu.cn>
2021-01-08 09:51:57 +0800

    rs

---
+++ base.h
@@ -0,0 +1,25 @@
+//*This is base apis that is requierd in this progarm
+//*author Keer M 2021.2
+
+#include<opencv2/opencv.hpp>
+#include<opencv2/core.hpp>
+#include<iostream>
+#include<array>
+#include<opencv2/highgui.hpp>
+#include<cstring>
+#include<string>
+#include<fstream>
+#include<
+
+#define MAX_CARD 9
+#define DPI 300
+#define PRINT_ROWS 2480
+#define PRINT_COLS 3508
+#define CARD_ROWS 697
+#define CARD_COLS 1016
+#define BRORDER_ROWS 97
+#define BROEDER_COLS 115
+
+using namespace std;
+using namespace cv;
+
